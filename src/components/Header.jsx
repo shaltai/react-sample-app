59d5b1bcdccb8898a5313ee8a738caac06e0a2c2
Stanislav [shaltai] Pimenov <yarsklad@gmail.com>
2020-01-26 23:16:31 +0300

    Lesson 15 — New Components, Basic CSS Improvements. Day 1.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Styles from './Header.module.css';
-import logo from '../logo.svg';
+import Logo from '../img/logo.svg';
 
 function Header() {
 	return (
 		<header className={Styles.header}>
-			<img className={Styles.logo} src={logo} alt="Logotype"/>
+			<img className={Styles.logo} src={Logo} alt="Logotype"/>
+			<div className={Styles.item}>
+				<a href="/#" className={Styles.link}>about min-i</a>
+			</div>
 		</header>
 	)
   }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,19 +5,19 @@ function Navbar() {
 	return (
 		<nav className={Styles.nav}>
 			<div className={Styles.item}>
-				<a href="/#" className={Styles.link}>Messages</a>
+				<a href="/#" className={Styles.link}>add to friendlist</a>
 			</div>
 			<div className={Styles.item}>
-				<a href="/#" className={Styles.link}>News</a>
+				<a href="/#" className={Styles.link}>write message</a>
 			</div>
 			<div className={Styles.item}>
-				<a href="/#" className={Styles.link}>Music</a>
+				<a href="/#" className={Styles.link}>notify about posts</a>
 			</div>
 			<div className={Styles.item}>
-				<a href="/#" className={Styles.link}>Profile</a>
+				<a href="/#" className={Styles.link}>invite to community</a>
 			</div>
 			<div className={Styles.item}>
-				<a href="/#" className={Styles.link}>Settings</a>
+				<a href="/#" className={Styles.link}>remove from feed</a>
 			</div>
 		</nav>
 	)
